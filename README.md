fb6a56fa1e7d518110838394369be38a65e50e02
Graham Gilbert <graham@grahamgilbert.com>
2014-01-26 12:13:30 +0000

    Mav Compatibility plugin for blog post.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 sal-plugins
 ===========
 
-Plugins for Sal (The Munki Puppet)
+Plugins for Sal (The Munki Puppet).
+
+## Installation
+
+Change into your ``plugins`` directory of your Sal install (if you followed the instructions provided with Sal, this will be ``/usr/local/sal_env/sal/plugins``), and then clone this repository as ``grahamgilbert``.
+
+``` bash
+$ cd /usr/local/sal_env/sal/plugins
+$ git clone https://github.com/grahamgilbert/sal-plugins.git grahamgilbert
+```
---
+++ mavcompatibility/mavcompatibility.py
@@ -0,0 +1,28 @@
+from yapsy.IPlugin import IPlugin
+from yapsy.PluginManager import PluginManager
+from django.template import loader, Context
+from django.db.models import Count
+from server.models import *
+
+class MavCompatibility(IPlugin):
+    def show_widget(self, page, machines=None, theid=None):
+
+        if page == 'front':
+            t = loader.get_template('grahamgilbert/mavcompatibility/templates/front.html')
+        
+        if page == 'bu_dashboard':
+            t = loader.get_template('grahamgilbert/mavcompatibility/templates/id.html')
+        
+        if page == 'group_dashboard':
+            t = loader.get_template('grahamgilbert/mavcompatibility/templates/id.html')
+            
+        
+        not_compatible = machines.filter(condition__condition_name='supported_major_os_upgrades').exclude(condition__condition_name='supported_major_os_upgrades', condition__condition_data__contains='10.9').count()
+        
+        c = Context({
+            'title': '10.9 Compatibility',
+            'not_compatible': not_compatible,
+            'page': page,
+            'theid': theid
+        })
+        return t.render(c), 3
